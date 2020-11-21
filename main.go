a0c6a829f7c822067ab09c0e56791d6ff8f5cac3
Tobias <bvrcreepyx@hotmail.de>
2020-11-21 17:40:40 +0100

    add logging configuration

--- main.go
+++ main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"flag"
+	"log"
 	"strconv"
 	"strings"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 var (
+	logLevel         = flag.Int("logLevel", 4, "log level (0-6)")
+	logReportCaller  = flag.Bool("logReportCaller", true, "log report caller")
+	logFormatterJSON = flag.Bool("logFormatterJson", false, "log formatter json")
 	blueprintImageId = flag.Int("blueprintImageId", 26408426, "blueprint image id")
 	locationName     = flag.String("locationName", "nbg1", "location name")
 	networkIds       = flag.String("networkIds", "194958", "comma separated list of network ids")
 	sshKeyIds        = flag.String("sshKeyIds", "2403353,2355137", "comma separated list if ssh key ids")
 )
 
-func main() {
+func init() {
 	flag.Parse()
+	logrus.SetLevel(logrus.Level(*logLevel))
+	logrus.SetReportCaller(*logReportCaller)
+	if *logFormatterJSON {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+	log.SetOutput(logrus.StandardLogger().Out)
+}
+
+func main() {
 	var networks []*hcloud.Network
 	var sshKeys []*hcloud.SSHKey
 	networkIdsSplit := strings.Split(*networkIds, ",")
 	for _, networkIdStr := range networkIdsSplit {
 		networkId, err := strconv.Atoi(networkIdStr)
 		if err != nil {
-			log.Fatalf("networkIds must be int")
+			logrus.Fatalf("networkIds must be int")
 		}
 		networks = append(networks, &hcloud.Network{ID: networkId})
 	}
@@ -32,7 +45,7 @@ func main() {
 	for _, sshKeyIdsStr := range sshKeyIdsSplit {
 		sshKeyId, err := strconv.Atoi(sshKeyIdsStr)
 		if err != nil {
-			log.Fatalf("sshKeyIds must be int")
+			logrus.Fatalf("sshKeyIds must be int")
 		}
 		sshKeys = append(sshKeys, &hcloud.SSHKey{ID: sshKeyId})
 	}
@@ -43,11 +56,11 @@ func main() {
 		sshKeys:        sshKeys,
 	})
 	if err != nil {
-		log.Fatalf("failed to create control: %w", err)
+		logrus.Fatalf("failed to create control: %w", err)
 	}
 
 	err = control.Run()
 	if err != nil {
-		log.Fatalf("control api failed: %s", err)
+		logrus.Fatalf("control api failed: %s", err)
 	}
 }
